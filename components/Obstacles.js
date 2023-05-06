0afbe0304f8353316ca12315f624c75222ba034a
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-05-06 18:03:45 -0500

    obstacles added

--- components/Obstacles.js
+++ components/Obstacles.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Obstacles = ({ obstaclesLeft, obstacleWidth, obstacleHeight, gap }) => {
+const Obstacles = ({ color, obstaclesLeft, obstacleWidth, obstacleHeight, gap }) => {
     return (
         <>
             <View style={{
                 position: 'absolute',
-                backgroundColor: 'green',
+                backgroundColor: color,
                 width: obstacleWidth,
                 height: obstacleHeight,
                 left: obstaclesLeft,
@@ -14,7 +14,7 @@ const Obstacles = ({ obstaclesLeft, obstacleWidth, obstacleHeight, gap }) => {
             }} />
             <View style={{
                 position: 'absolute',
-                backgroundColor: 'green',
+                backgroundColor: color,
                 width: obstacleWidth,
                 height: obstacleHeight,
                 left: obstaclesLeft,
